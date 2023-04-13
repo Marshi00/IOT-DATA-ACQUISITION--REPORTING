36701b23c6357eb24f80a69a15243d4055528712
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2023-04-13 14:49:57 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+from pycomm3 import LogixDriver
+
+# set up the driver
+with LogixDriver('192.168.1.1') as plc:
+    # read the tag 'MyTag' from the PLC
+    tag_value = plc.read('MyTag')
+
+    # print the value of the tag to the console
+    print(f"The value of 'MyTag' is: {tag_value}")
+"""
+from pylogix import PLC
+
+# set up the connection to the PLC
+with PLC() as plc:
+    plc.IPAddress = '192.168.0.99'
+    connected = plc.IPAddress
+
+    if connected:
+        # read the tag 'MyTag' from the PLC
+        response = plc.Read('report15_data[0].log_data[0].total')
+
+        # check if the read was successful
+        if response.Status == "Success":
+            # print the value of the tag to the console
+            print(f"The value of 'MyTag' is: {response.Value}")
+        else:
+            print("Read failed")
+    else:
+        print("Could not connect to PLC")
