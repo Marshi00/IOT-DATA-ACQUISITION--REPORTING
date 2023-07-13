9601dcfc03003f9e4ff5d295a1206c9509a5024d
Marshi00 <112508169+Marshi00@users.noreply.github.com>
2023-07-12 23:52:46 -0400

    Todo added

--- collection_sys_v2.py
+++ collection_sys_v2.py
@@ -139,6 +139,7 @@ if engine is not False:
             else:
                 logging.info("Could not connect to PLC")
                 print("Could not connect to PLC")
+                # TODO : test the exit
     except Exception as e:
         print(e)
         print("failed to do the data data extraction , Main loop")
